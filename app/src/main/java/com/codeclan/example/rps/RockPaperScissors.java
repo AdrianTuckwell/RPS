d8c882f5666cb73570d902e4ec55694d4d95c4bf
Adrian Tuckwell <adrian.tuckwell@gmail.com>
2016-11-10 09:53:00 +0000

    refactored RPS class

--- app/src/main/java/com/codeclan/example/rps/RockPaperScissors.java
+++ app/src/main/java/com/codeclan/example/rps/RockPaperScissors.java
@@ -15,57 +15,73 @@ import java.util.Random;
  * Created by user on 09/11/2016.
  */
 
-public class RockPaperScissors {
+public class RockPaperScissors
+{
 
-    //    EditText mPlayerName;
     HashMap<String, String> moves = new HashMap<>();
     String mPlayerMove;
     String mComputerMove;
 
-
-
-    public RockPaperScissors(String move) {
+    public RockPaperScissors(String move)
+    {
         this.mPlayerMove = move;
         this.mComputerMove = null;
         setUpMoves();
     }
 
-
-    public void setUpMoves() {
-        moves.put("Scissors", "Paper");
-        moves.put("Paper", "Rock");
-        moves.put("Rock", "Scissors");
+    // Hash map of winning moves...
+    public void setUpMoves()
+    {
+        //         Key...      Value...
+        moves.put("Scissors", "Paper");     // Scissors beats Paper
+        moves.put("Paper", "Rock");         // Paper beats Rock
+        moves.put("Rock", "Scissors");      // Rock beats Scissors
     }
 
-    public void computerMove() {
+    public void computerMove()
+    {
+        // create a random computer move...
+        // with a random object and Array list to hold moves
         Random rand = new Random();
         ArrayList<String> movesArray = new ArrayList<>();
-        for(String key : moves.keySet()) {
+
+        for(String key : moves.keySet())
+        {
             movesArray.add(key);
         }
+
         int randomIndex = rand.nextInt(3);
+
         String randomAnswer = movesArray.get(randomIndex);
+
         this.mComputerMove = randomAnswer;
     }
 
-    public boolean win() {
+    public boolean win()
+    {
         return (moves.get(mPlayerMove).equals(mComputerMove));
     }
 
-    public boolean lose(){
+    public boolean lose()
+    {
         return (moves.get(mComputerMove).equals((mPlayerMove)));
     }
 
-    public String getResult(){
+    public String getResult()
+    {
+
         computerMove();
         String outcome = null;
-        if( win()){
+        if( win())
+        {
             outcome = "You win!";
         }
-        else if(lose()){
+        else if(lose())
+        {
             outcome = "Computer wins!";
         }
-        else{
+        else
+        {
             outcome = "It's a draw!";
         }
         return outcome;
